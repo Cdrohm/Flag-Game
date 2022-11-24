63c610f805c1d62248875392095306686c17af09
Clément Théraud <theraud.clement@gmail.com>
2022-11-24 10:50:50 +0100

    start + clean + routes

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A Propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi dicta cupiditate, quia, voluptates obcaecati vel aspernatur natus tempora rerum asperiores quos mollitia recusandae quaerat delectus ex harum quis quod et. Dolores incidunt voluptas, nisi animi nemo aut? Obcaecati velit, vitae suscipit deleniti eligendi culpa illum ipsa asperiores autem quaerat excepturi perferendis. Molestiae possimus voluptatem neque adipisci illo animi error? Unde deserunt velit, aperiam quae labore soluta eligendi consequuntur libero perferendis necessitatibus sapiente reprehenderit sed, corporis eius ex praesentium facere molestiae ipsam iusto dolore? Ab amet laudantium odit earum magnam. Tenetur dolorem at vel dolores quo esse ex adipisci architecto dicta.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eius facilis velit ab. Aliquam dolores ex molestias quos excepturi, eaque ad explicabo accusantium tempore est possimus cupiditate quam molestiae adipisci!</p>
+        </div>
+    );
+};
+
+export default About;
